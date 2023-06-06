abce908db87bd79f57f4a11ce525cc620297e5db
Dachev <dachev1919@gmail.com>
2023-06-06 18:13:07 +0300

    added redux store

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categoriesSlice from "./categories/categoriesSlice";
+
+export const store = configureStore({
+  reducer: {
+    categories: categoriesSlice,
+  }
+});
